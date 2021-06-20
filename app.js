864446966179d428e26259fa37ef4a49f94ad09e
micha-bitton-walkme <micha.bitton@walkme.com>
2021-06-20 21:44:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function find() {
             : '<p><b>אין תוצאות</b></p><hr /><p>אנא בדוק שאין טעות בשם.<br> יש להקליד בעברית<br> במידה ולא נמצא לך שולחן, תרגיש חופשי לשבת במקומות הלא מסומנים, תודה שהגעת&#128519 </p>';
 
         if (matches.length > 3) html += `<p><small> ועוד ${matches.length - 3} התאמות</small></p>`;
-        if (matches.length > 1) html += `<hr /></p>${footer}</p>`;
+        if (matches.length >= 1) html += `<hr /></p><b>${footer}</b></p>`;
     } else {
         html = '<p>הזן שם ולחץ על "חפש"</p>';
     }
